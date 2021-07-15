626c7495bb5bdddfa8e1097fb6d8d1d9599860cc
Hrishav Gajurel <hrishavgl@gmail.com>
2021-07-15 16:59:47 +0545

    clear buttons work now

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
-function difficulty(value) {
-    var easy = document.getElementById("easy");
-    var hard = document.getElementById("hard");
+function setDifficulty(diff) {
+    easy = easyButton;
+    hard = hardButton;
 
-    if(value == "easyMode") {
+    if(diff == "easyMode") {
         easy.style.color = "red";
         hard.style.color = "black";
-    } else if(value == "hardMode") {
+    } else if(diff == "hardMode") {
         hard.style.color = "red";
         easy.style.color = "black";
     }
+}
+
+function clearOutput() {
+    outputArea.textContent = "";
+}
+
+function removeLast() {
+    outputArea.textContent = outputArea.textContent.slice(0, -1);
+}
+
+function calculation(val) {
+    alert(val);
 }
