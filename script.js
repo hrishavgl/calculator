7d48e3cc157c7133e05b2951557ba610dcd852c0
Hrishav Gajurel <hrishavgl@gmail.com>
2021-07-18 12:12:05 +0545

    shuffled buttons

--- script.js
+++ script.js
@@ -27,5 +27,24 @@ function setDifficulty(diff) {
 }
 
 function randomizeButtons() {
-    alert("still under construction");
+    buttons = [b01.textContent, b02.textContent, b03.textContent, b04.textContent, b05.textContent, b06.textContent, b07.textContent, b08.textContent, b09.textContent, b10.textContent, b11.textContent, b12.textContent, b13.textContent, b14.textContent, b15.textContent, b16.textContent];
+
+    buttons.sort(() => Math.random() - 0.5)
+
+    b01.textContent = buttons[0];
+    b02.textContent = buttons[1];
+    b03.textContent = buttons[2];
+    b04.textContent = buttons[3];
+    b05.textContent = buttons[4];
+    b06.textContent = buttons[5];
+    b07.textContent = buttons[6];
+    b08.textContent = buttons[7];
+    b09.textContent = buttons[8];
+    b10.textContent = buttons[9];
+    b11.textContent = buttons[10];
+    b12.textContent = buttons[11];
+    b13.textContent = buttons[12];
+    b14.textContent = buttons[13];
+    b15.textContent = buttons[14];
+    b16.textContent = buttons[15];
 }
